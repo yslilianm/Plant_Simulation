d837900d9d7367545462f625fa0074e6b7c4efac
yslilianm <yslilianm@gmail.com>
2019-04-19 23:46:08 -0400

    Updated app; Setup initial state of cell

--- js/app.js
+++ js/app.js
@@ -1,30 +1,53 @@
-// Create three things: scene, camera and renderer
 const scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer()
 renderer.setSize( window.innerWidth, window.innerHeight);
 
-//Not sure what is this for
 document.body.appendChild( renderer.domElement );
 
-//Create geometry, material and mesh
-var geometry = new THREE.Geometry();
+// Initial state /////
+// Create a base
+var geometryList = [];
+var base = new THREE.SphereGeometry( 50, 5, 5 );
+let verticesList = base.vertices;
 
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// Create a cell
+var cell = new THREE.SphereGeometry( 5, 4, 4 );
 
-camera.position.z = 5;
+// Define material
+var material = new THREE.MeshBasicMaterial( { color: 0x2194ce, wireframe:true } );
+
+// Create mesh for cells
+// Change the position of cells
+for (let vertex of verticesList){
+	var cellMesh = new THREE.Mesh( cell, material );
+	cellMesh.position.x = vertex.x;
+	cellMesh.position.y = vertex.y;
+	cellMesh.position.z = vertex.z;
+	scene.add(cellMesh);
+}
+
+// Create mesh for the base
+	var baseMesh = new THREE.Mesh( base, material );
+	scene.add(baseMesh);
+
+
+camera.position.z = 120;
 
 //Rendering the scene
 var animate = function () {
 	//Create a loop that causes the renderer to draw the scene every time the screen is refreshed
 	requestAnimationFrame( animate );
 
-    //Animating the cube
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-    renderer.render( scene, camera );
+	//Animating the cube
+	// for (let geo of geometryList){
+	// 	cube.rotation.x += 0.01;
+	//   cube.rotation.y += 0.01;
+	// }
+
+	renderer.render( scene, camera );
 };
 animate();
+
+
